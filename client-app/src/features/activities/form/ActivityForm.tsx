c7cbc6662065500fb01ad822752deef477901a9e
Krishna-RP <71462185+Krishna-RP@users.noreply.github.com>
2021-03-30 20:19:08 -0700

    adding Routing to the application

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,13 +1,19 @@
 import { observer } from "mobx-react-lite";
-import React, { ChangeEvent, useState } from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
+import { Link, useHistory, useParams } from "react-router-dom";
 import { Button, Form, Segment } from "semantic-ui-react";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { useStore } from "../../../app/stores/store";
+import { v4 as uuid } from "uuid";
+
 
 export default observer (function ActivityForm() {  
+  const history = useHistory();
   const {activityStore}= useStore();
-  const {selectedActivity,closeForm, createActivity ,updateActivity,loading} = activityStore;
+  const {createActivity ,updateActivity,loading, loadActivity, loadingInitial} = activityStore;
+  const{id} = useParams<{id: string}>();
 
-  const intialState = selectedActivity ?? {
+  const [activity, setActivity] = useState({
     id: "",
     title: "",
     category: "",
@@ -15,12 +21,22 @@ export default observer (function ActivityForm() {
     date: "",
     venue: "",
     city: "",
-  };
-
-  const [activity, setActivity] = useState(intialState);
+  });
+  
+  useEffect(() => {
+    if (id) loadActivity(id).then(activity => setActivity(activity!))
+    },[id, loadActivity]);
 
   function handleSubmit() {
-    activity.id ? updateActivity(activity) : createActivity(activity);
+   if(activity.id.length === 0) {
+     let newActivity= {
+       ...activity,
+       id: uuid()
+     };
+     createActivity(newActivity).then(() => history.push(`/activities/${newActivity.id}`))
+   } else{
+     updateActivity(activity).then(() => history.push(`/activities/${id}`))
+   }
     }
 
   function handleInputChange(
@@ -29,6 +45,8 @@ export default observer (function ActivityForm() {
     const { name, value } = event.target;
     setActivity({ ...activity, [name]: value });
   }
+
+  if(loadingInitial) return<LoadingComponent content='Loading activity...'/>
 
   return (
     <Segment clearing>
@@ -77,7 +95,7 @@ export default observer (function ActivityForm() {
           type="submit"
           content="Submit"
         />
-        <Button onClick={closeForm} floated="right" content="Cancel" />
+        <Button as={Link} to={`/activities`} floated="right" content="Cancel" />
       </Form>
     </Segment>
   );
