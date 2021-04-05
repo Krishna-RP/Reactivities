8bd5afeec355e974ebdbe418ccc49574a17f4c7c
Krishna-RP <71462185+Krishna-RP@users.noreply.github.com>
2021-04-04 20:12:32 -0700

    Error handling - II

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,20 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./app/layout/styles.css";
-import 'react-calendar/dist/Calendar.css'
+import "react-calendar/dist/Calendar.css";
+import "react-toastify/dist/ReactToastify.min.css";
 import App from "./app/layout/App";
 import reportWebVitals from "./reportWebVitals";
 import "semantic-ui-css/semantic.min.css";
 import { store, StoreContext } from "./app/stores/store";
-import { BrowserRouter } from "react-router-dom";
+import { Router } from "react-router-dom";
+import {createBrowserHistory} from'history';
+
+export const history = createBrowserHistory(); 
 
 ReactDOM.render(
-  <StoreContext.Provider value={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StoreContext.Provider>,
-  document.getElementById("root")
+   <StoreContext.Provider value={store}>
+      <Router history={history}>
+         <App />
+      </Router>
+   </StoreContext.Provider>,
+   document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
