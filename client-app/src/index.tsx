383129d0b3aa5b052c564cdaa52fbd1a8ede8d68
Krishna-RP <71462185+Krishna-RP@users.noreply.github.com>
2021-04-05 17:02:05 -0700

    Adding formik for forms and form validations

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -2,15 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./app/layout/styles.css";
 import "react-calendar/dist/Calendar.css";
+import "react-datepicker/dist/react-datepicker.css";
 import "react-toastify/dist/ReactToastify.min.css";
 import App from "./app/layout/App";
 import reportWebVitals from "./reportWebVitals";
 import "semantic-ui-css/semantic.min.css";
 import { store, StoreContext } from "./app/stores/store";
 import { Router } from "react-router-dom";
-import {createBrowserHistory} from'history';
+import { createBrowserHistory } from "history";
 
-export const history = createBrowserHistory(); 
+export const history = createBrowserHistory();
 
 ReactDOM.render(
    <StoreContext.Provider value={store}>
