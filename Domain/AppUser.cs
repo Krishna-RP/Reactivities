0a881aa220507fa2a97d7e9439d852a14f2658e9
Krishna-RP <71462185+Krishna-RP@users.noreply.github.com>
2021-05-07 21:20:15 -0700

    Adding Image upload API using Cloudinary

--- Domain/AppUser.cs
+++ Domain/AppUser.cs
@@ -3,13 +3,15 @@ using Microsoft.AspNetCore.Identity;
 
 namespace Domain
 {
-      public class AppUser : IdentityUser
+      public class AppUser : IdentityUser 
       {
             public string DisplayName { get; set; }
 
             public string Bio { get; set; }
 
             public ICollection<ActivityAttendee> Activities { get; set; }   
+
+            public ICollection<Photo> Photos { get; set; }
 
       }
 }
